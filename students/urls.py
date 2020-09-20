01d6adf1611ac942f48a2b9561090de5279920be
Joy <ziaurjoy802@gmail.com>
2020-09-21 00:44:08 +0600

    "teacher app done"

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create', views.create_student, name = 'create-student'),
-    
+    path('create', views.CreateStudent.as_view(), name = 'create-student'),
+    # path('create', views.create_student, name = 'create-student'),
+
 ]
--- students/views.py
+++ students/views.py
@@ -2,22 +2,35 @@ from django.shortcuts import render
 
 from .models import StudentInfo
 from .forms import StudentCreateForms
+
+from django.views.generic import CreateView
+
+
 # Create your views here.
 
 
+class CreateStudent(CreateView):
+    model = StudentInfo
+    template_name = 'student/create.html'
+    fields = "__all__"
+    success_url = 'student-list'
 
-def create_student(request):
-    forms = StudentCreateForms()
-    if request.method == 'POST':
-        forms = StudentCreateForms(request.POST)
-        if forms.is_valid():
-            name = forms.cleaned_data['name']
-            age = forms.cleaned_data['age']
-            gender = forms.cleaned_data['gender']
-            StudentInfo.objects.create(
-                name = name,
-                age = age,
-                gender = gender
-            )
-    context = {'forms': forms}
-    return render(request,'student/create.html',context)
+
+
+# def create_student(request):
+#     forms = StudentCreateForms()
+#     if request.method == 'POST':
+#         forms = StudentCreateForms(request.POST)
+#         if forms.is_valid():
+#             name = forms.cleaned_data['name']
+#             age = forms.cleaned_data['age']
+#             gender = forms.cleaned_data['gender']
+#             StudentInfo.objects.create(
+#                 name = name,
+#                 age = age,
+#                 gender = gender
+#             )
+#     context = {'forms': forms}
+#     return render(request,'student/create.html',context)
+
+
